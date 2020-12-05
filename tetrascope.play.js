99a9a962ad379fea12589d25e29dabcf82616cca
4eckme <60138564+4eckme@users.noreply.github.com>
2020-12-05 03:57:22 +0300

    Tetrascope fractal animation
    Tetrascope fractal animation

--- tetrascope.play.js
+++ tetrascope.play.js
@@ -2,9 +2,8 @@
 // Availiable to execute in browser console
 // For base tetrascope image set R=1024, n=1
 (function(){ var R = 256; var D=2*R+1;
-var pow = Math.pow; var floor=Math.floor;
-var ceil_log16 = function(col) { return col.toString(16).length; }
-rgb = function(x, y){ return floor(   (x*x*n+y*y*n)*pow(16, ( 6-ceil_log16(x*x*n+y*y*n)) )   ); }
+var pow = Math.pow; var floor=Math.floor; var ceil=Math.ceil; var log2 = Math.log2;
+var rgb = function(x, y){ return floor(   (x*x*n+y*y*n)*pow(16, ( 6-ceil(log2(x*x*n+y*y*n)/4)) )   ); }
 document.body.innerHTML=('<canvas id="C" width="'+D+'" height="'+D+'"></canvas>');
 var canvas = document.getElementById('C');
 var ctx = canvas.getContext('2d');
