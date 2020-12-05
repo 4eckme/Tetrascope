dca4df9c2aa2d4c9f29b22f42e082c75bcc9a349
4eckme <60138564+4eckme@users.noreply.github.com>
2020-12-05 06:39:48 +0300

    Tetrascope sample
    Tetrascope sample

---
+++ tetrascope.sample.py
@@ -0,0 +1,40 @@
+// Tetrascope sample
+// R=1024, scale=100%
+
+import numpy as np
+from PIL import Image
+ 
+R = 1024
+D = 2*R+1
+ 
+def hex_to_rgb(value):
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+ 
+def hex_to_str(c):
+    c = c.lstrip('0x')
+    if len(c) <= 6:
+        return c.ljust(6, '0')
+    else:
+        return c.substring(0, 6)
+ 
+data = np.zeros((D,D,3), dtype=np.uint8)
+sqrR = R*R
+for x in range(0, R):
+    if not x % 128:
+        print(x, " rows rendered")
+    for y in range (x, R):
+        sqr = (x*x+y*y)
+        if sqr <= sqrR:
+            rgbtuple = hex_to_rgb(hex_to_str(hex(sqr)))
+            data[R+x,R+y] = rgbtuple
+            data[R+x,R-y] = rgbtuple
+            data[R-x,R+y] = rgbtuple
+            data[R-x,R-y] = rgbtuple
+            data[R+y,R+x] = rgbtuple
+            data[R+y,R-x] = rgbtuple
+            data[R-y,R+x] = rgbtuple
+            data[R-y,R-x] = rgbtuple
+ 
+img = Image.fromarray(data)
+img.save("tetrascope.png")
